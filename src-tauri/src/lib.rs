061ab3beaa98aee8af936393b9344fbc27530639
Your Name <88072010+Razshy@users.noreply.github.com>
2026-04-06 18:06:29 -0500

    ci: add release pipeline, restore updater, add app metadata

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -386,6 +386,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .manage(Arc::new(TerminalStore::new()))
         .manage(Arc::new(ProcessStore::new()))
         .manage(Arc::new(DebugAdapterStore::new()))
